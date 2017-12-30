a1fd26bd8e9d748c56307b82f71a762abeb45fde
Andrea Chen <andreachen.ac@gmail.com>
2017-12-29 21:21:06 -0800

    Create db seeds for bins and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#/db/seeds.rb
+require 'open-uri' # Needed to open web urls
+
+################### BIN SEEDS #####################
+Bin.delete_all
+
+URL = "http://gisrevprxy.seattle.gov/arcgis/rest/services/SDOT_EXT/ASSETS/MapServer/11/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson"
+
+puts "@@@@@@@@@@@@@ about to call URL @@@@@@@@@@@@@@@"
+response = HTTParty.get(URL)
+parsed_response = JSON.parse(response)
+
+if parsed_response["features"] == 0
+  puts "SEEDING FAILED, no results"
+else
+  bins = parsed_response["features"].map do |feature|
+    bin = Bin.create!({
+      bin_type: feature["attributes"]["UNITDESC"],
+      latitude: feature["attributes"]["SHAPE_LAT"],
+      longitude: feature["attributes"]["SHAPE_LNG"]
+      })
+  end
+  puts "SEEDING SUCCESSFUL?"
+end
+
+################### USER SEEDS #####################
+puts "@@@@@@@@@@@@ Creating users @@@@@@@@@@@@@@@"
+users = User.create!([{ username: 'ac', email: 'a@c.com', password: '123' }, { username: 'ace', email: 'a@ce.com', password: '456' }])
+puts "Successfully created users?"
