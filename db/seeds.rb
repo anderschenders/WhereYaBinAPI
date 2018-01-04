40c357d520786d56355c8d903488373bbdf3c677
Andrea Chen <andreachen.ac@gmail.com>
2018-01-04 11:43:53 -0800

    Modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,68 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-#/db/seeds.rb
-require 'open-uri' # Needed to open web urls
-
-################### BIN SEEDS #####################
-Bin.delete_all
-
-URL = "http://gisrevprxy.seattle.gov/arcgis/rest/services/SDOT_EXT/ASSETS/MapServer/11/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson"
-
-puts "@@@@@@@@@@@@@ about to call URL @@@@@@@@@@@@@@@"
-response = HTTParty.get(URL)
-parsed_response = JSON.parse(response)
-
-if parsed_response["features"] == 0
-  puts "SEEDING FAILED, no results"
-else
-  bins = parsed_response["features"].map do |feature|
-    bin = Bin.create!({
-      bin_type: feature["attributes"]["UNITDESC"],
-      latitude: feature["attributes"]["SHAPE_LAT"],
-      longitude: feature["attributes"]["SHAPE_LNG"]
-      })
-  end
-  puts "SEEDING SUCCESSFUL?"
-end
-
 ################### USER SEEDS #####################
 puts "@@@@@@@@@@@@ Creating users @@@@@@@@@@@@@@@"
-users = User.create!([{ username: 'ac', email: 'a@c.com', password: '123' }, { username: 'ace', email: 'a@ce.com', password: '456' }])
-puts "Successfully created users?"
+users = User.create!([{ username: 'ac', email: 'A@c.com', password: '123' }, { username: 'ace', email: 'A@ce.com', password: '456' }])
+puts "Successfully created users!"
+
+################# BIN JSON FILE #####################
+require 'json'
+
+puts "@@@@@@@@@@@ Creating bins @@@@@@@@@@@@@@@"
+
+BIN_FILE = Rails.root.join('db', 'seed_data', 'citybindata.json')
+puts "Loading raw bin data from #{BIN_FILE}"
+
+file = File.read(BIN_FILE)
+parsed_file = JSON.parse(file)
+
+bin_failures = []
+
+if parsed_file["features"] == 0
+  puts "SEEDING FAILED, no results"
+else
+  parsed_file["features"].map do |feature|
+    bin = Bin.new({
+      bin_type: feature["attributes"]["PC_SVC_TYPE"],
+      latitude: feature["attributes"]["PC_LAT_COORD"],
+      longitude: feature["attributes"]["PC_LONG_COORD"]
+    })
+    successful = bin.save
+    if !successful
+      bin_failures << bin
+    end
+  end
+
+  puts "Successfully created bins!!"
+end
+
+################## USER_BIN SEEDS ##################
+# puts "@@@@@@@@@@@ Creating user-bins @@@@@@@@@@@@@@@"
+# user_bins = UserBin.create!([{ user_id: 1, bin_id: 1 }, { user_id: 1, bin_id: 2 }])
+# puts "Successfully created user-bins!"
+
+
+
+
+############ CALLING CITY PAYSTATION API  ###########
+# require 'open-uri' # Needed to open web urls
+
+################### BIN SEEDS #####################
+# Bin.delete_all
+#
+# URL = "http://gisrevprxy.seattle.gov/arcgis/rest/services/SDOT_EXT/ASSETS/MapServer/11/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson"
+#
+# puts "@@@@@@@@@@@@@ about to call URL @@@@@@@@@@@@@@@"
+# response = HTTParty.get(URL)
+# parsed_response = JSON.parse(response)
+#
+# if parsed_response["features"] == 0
+#   puts "SEEDING FAILED, no results"
+# else
+#   bins = parsed_response["features"].map do |feature|
+#     bin = Bin.create!({
+#       bin_type: feature["attributes"]["UNITDESC"],
+#       latitude: feature["attributes"]["SHAPE_LAT"],
+#       longitude: feature["attributes"]["SHAPE_LNG"]
+#       })
+#   end
+#   puts "SEEDING SUCCESSFUL?"
+# end
