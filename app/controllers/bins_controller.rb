f415e0bfb83112141ab42809989b774660086d4e
Andrea Chen <andreachen.ac@gmail.com>
2018-01-06 13:24:33 -0800

    Add logic to BinsController#index to format data according to locations

--- app/controllers/bins_controller.rb
+++ app/controllers/bins_controller.rb
@@ -1,9 +1,27 @@
 class BinsController < ApplicationController
 
   def index
-    data = Bin.all
+    all_bins = Bin.all
 
-    render status: :ok, json: data
+    #organize all_bins into arrays of arrays with same locations together
+    unique = true
+    all_bins_arrays = all_bins.each_slice(1).to_a
+    unique_locations_format = []
+
+    all_bins_arrays.each do |bin|
+      unique_locations_format.each do |b|
+        if b[0].location == bin[0].location
+          b << bin[0]
+          unique = false
+        end
+      end
+      if unique == true
+        unique_locations_format << bin
+      end
+      unique = true
+    end
+
+    render status: :ok, json: unique_locations_format
   end
 
 end
