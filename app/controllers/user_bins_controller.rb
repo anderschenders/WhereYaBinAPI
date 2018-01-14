299c8e360dff33110582e60ccbbf97c9a7e1556f
Andrea Chen <andreachen.ac@gmail.com>
2018-01-13 18:25:40 -0800

    Modify json response for UserBinsController#create to include user_bins

--- app/controllers/user_bins_controller.rb
+++ app/controllers/user_bins_controller.rb
@@ -119,31 +119,37 @@ class UserBinsController < ApplicationController
     new_user_bin = UserBin.new(
       user_id: params[:user_id],
       bin_id: params[:bin_id],
-      action: params[:userAction],
+      action: params[:user_action],
       user_lat: params[:user_lat],
       user_lng: params[:user_lng]
     )
-
-    # get total distance user has travelled
-    # total_distance_travelled = @user.total_distance_travelled
-    #
-    # json_response = {
-    #   new_user_bin: new_user_bin,
-    #   updated_user: @user,
-    #   total_dist: total_distance_travelled,
-    #   bin_location: "#{bin_latitude},#{bin_longitude}"
-    # }
 
     if new_user_bin.save
       # get total distance user has travelled
       @user.reload
       total_distance_travelled = @user.total_distance_travelled
 
+      #get all user_bins
+      user_bins = UserBin.where(user_id: @user.id)
+      user_bins_sorted = user_bins.order(:created_at).reverse
+
+      user_bins_array = user_bins_sorted.each_slice(1).to_a
+
+      user_bins_array.each do |user_bin|
+        bin = Bin.find_by(id: user_bin[0].bin_id)
+        if bin.bin_type === "RYPUBL"
+          user_bin << { "bin_type" => "RECYCLING" }
+        else
+          user_bin << { "bin_type" => "GARBAGE" }
+        end
+      end
+
       json_response = {
         new_user_bin: new_user_bin,
         updated_user: @user,
         total_dist: total_distance_travelled,
-        bin_location: "#{bin_latitude},#{bin_longitude}"
+        bin_location: "#{bin_latitude},#{bin_longitude}",
+        user_bins: user_bins_array
       }
 
       render status: :ok, json: json_response
