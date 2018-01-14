d335cff28f927c4ba8908990d22b63fd65be1d7e
Andrea Chen <andreachen.ac@gmail.com>
2018-01-13 20:00:12 -0800

    Refactor UsersController. Move code to get all user_bins to private method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,75 +1,34 @@
 class UsersController < ApplicationController
 
   def index
-    # request coming from profile page
-    if params[:id] != nil
-      @user = User.find_by(id: params[:id])
-
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
       # get total distance user has travelled
       total_distance_travelled = @user.total_distance_travelled
 
+      #get all user_bins
+      user_bins_array = get_user_bins(@user)
+
       json_response = {
         user: @user,
-        total_dist: total_distance_travelled
+        total_dist: total_distance_travelled,
+        user_bins: user_bins_array
       }
 
       render status: :ok, json: json_response
-    else # request coming from signin
-      @user = User.find_by(email: params[:email], password: params[:password])
-      if @user
-        # get total distance user has travelled
-        total_distance_travelled = @user.total_distance_travelled
-
-        #get all user_bins
-        user_bins = UserBin.where(user_id: @user.id)
-        user_bins_sorted = user_bins.order(:created_at).reverse
-
-        user_bins_array = user_bins_sorted.each_slice(1).to_a
-
-        user_bins_array.each do |user_bin|
-          bin = Bin.find_by(id: user_bin[0].bin_id)
-          if bin.bin_type === "RYPUBL"
-            user_bin << { "bin_type" => "RECYCLING" }
-          else
-            user_bin << { "bin_type" => "GARBAGE" }
-          end
-        end
-
-        # get bin type of each userbin and add to response
-        # user_bins.each do |user_bin|
-        #   bin = Bin.find_by(id: user_bin.bin_id)
-        #   if bin.bin_type === "RYPUBL"
-        #     user_bin["bin_type"] = "RECYCLING"
-        #   else
-        #     user_bin["bin_type"] = "GARBAGE"
-        #   end
-        # end
-
-        # render status: :ok, json: user_bins_array
-
-        json_response = {
-          user: @user,
-          total_dist: total_distance_travelled,
-          user_bins: user_bins_array
-        }
-
-        render status: :ok, json: json_response
+    else
+      user = User.find_by(email: params[:email])
+      if user.nil?
+        render status: :bad_request, json: { error: 'Invalid email. Please try again.' }
       else
-        user = User.find_by(email: params[:email])
-        if user.nil?
-          render status: :bad_request, json: { error: 'Invalid email. Please try again.' }
-        else
-          render status: :bad_request, json: { error: 'Invalid password. Please try again.' }
-        end
+        render status: :bad_request, json: { error: 'Invalid password. Please try again.' }
       end
-
     end
-
 
   end
 
   def create
-    # user sign up
+    # new user sign up
     @user = User.new({
       email: params[:email],
       username: params[:username],
@@ -77,24 +36,11 @@ class UsersController < ApplicationController
     })
 
     if @user.save
-
       # get total distance user has travelled
       total_distance_travelled = @user.total_distance_travelled
 
       #get all user_bins
-      user_bins = UserBin.where(user_id: @user.id)
-      user_bins_sorted = user_bins.order(:created_at).reverse
-
-      user_bins_array = user_bins_sorted.each_slice(1).to_a
-
-      user_bins_array.each do |user_bin|
-        bin = Bin.find_by(id: user_bin[0].bin_id)
-        if bin.bin_type === "RYPUBL"
-          user_bin << { "bin_type" => "RECYCLING" }
-        else
-          user_bin << { "bin_type" => "GARBAGE" }
-        end
-      end
+      user_bins_array = get_user_bins(@user)
 
       json_response = {
         user: @user,
@@ -108,4 +54,25 @@ class UsersController < ApplicationController
     end
 
   end
+end
+
+private
+
+def get_user_bins(user)
+
+  user_bins = UserBin.where(user_id: @user.id)
+  user_bins_sorted = user_bins.order(:created_at).reverse
+
+  user_bins_array = user_bins_sorted.each_slice(1).to_a
+
+  user_bins_array.each do |user_bin|
+    bin = Bin.find_by(id: user_bin[0].bin_id)
+    if bin.bin_type === "RYPUBL"
+      user_bin << { "bin_type" => "RECYCLING" }
+    else
+      user_bin << { "bin_type" => "GARBAGE" }
+    end
+  end
+
+  return user_bins_array
 end
