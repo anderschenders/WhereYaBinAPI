e4ed76c99a5522fda4aab67ab63405612a40b957
Andrea Chen <andreachen.ac@gmail.com>
2018-01-15 11:11:21 -0800

    Add preliminary tests for UserController methods

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,50 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "should respond with success when given valid data" do
+      params = {
+        email: "anders@chenders.com",
+        password: "meow"
+      }
+
+      get users_path, params: params
+      must_respond_with :success
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "should respond with bad_request when given invalid data" do
+      params = {
+        email: "invalid@email.com",
+        password: "meow"
+      }
+
+      get users_path, params: params
+      must_respond_with :bad_request
+    end
+  end
+
+  describe "create" do
+    it "should respond with success when given valid data" do
+      params = {
+        username: "newuser",
+        email: "new@user.com",
+        password: "new"
+      }
+
+      post users_path, params: params
+      must_respond_with :success
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "should respond with bad_request when given invalid data" do
+      params = {
+        username: "newuser2",
+        password: "meow"
+      }
+
+      post users_path, params: params
+      must_respond_with :bad_request
+    end
+  end
 end
